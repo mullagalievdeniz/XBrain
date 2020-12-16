849fbdce6675dfebf81740144d9eb6998b0a4a57
deniz_000 <mullagalievdeniz@yandex.ru>
2020-12-16 18:39:58 +0300

    XBRAIN

--- app/src/main/java/com/example/xbrain/Assistant_activivty.java
+++ app/src/main/java/com/example/xbrain/Assistant_activivty.java
@@ -3,12 +3,20 @@ package com.example.xbrain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Assistant_activivty extends AppCompatActivity {
+
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assistant_activivty);
+        btn = (Button) findViewById(R.id.button);
+        btn.s
     }
+
+
 }
