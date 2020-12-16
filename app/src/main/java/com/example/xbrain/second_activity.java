3171eda378e1c6105cc45a3c38dbb343e7bbc2f2
deniz_000 <mullagalievdeniz@yandex.ru>
2020-12-16 18:47:52 +0300

    XBRAIN

---
+++ app/src/main/java/com/example/xbrain/second_activity.java
@@ -0,0 +1,15 @@
+package com.example.xbrain;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedOnInstanceState){
+        super.onCreate();
+    }
+
+
+}
